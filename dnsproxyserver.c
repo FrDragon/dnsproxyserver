c03f7c05c75cdcf2322b9b12b84fb77da3765e3c
Gorovoy Ivan <navigora2000x@gmail.com>
2024-05-30 20:45:16 +0200

    DNS packet format realization

--- dnsproxyserver.c
+++ dnsproxyserver.c
@@ -8,34 +8,63 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+#define PORT 8053
+
+#define DNS_PACKET_SIZE 512
+
+#define QR_QUERY 0
+#define QR_RESPONSE 1
+
+#define OPCODE_QUERY 0
+#define OPCODE_IQUERY 1
+#define OPCODE_STATUS 2
+
+#define AA_NONAUTHORITY 0
+#define AA_AUTHORITY 1
+
 char upstreamServer[16];
 char blackList[256];
 char code[16];
 
 typedef struct {
-    unsigned short id;
-    unsigned char rd :1;
-    unsigned char tc : 1;
-    unsigned char aa :1;
-    unsigned char opcode :4;
-    unsigned char qr :1;
+    u_int16_t id;
 
-    unsigned char rcode :4;
-    unsigned char cd :1;
-    unsigned char ad :1;
-    unsigned char z :1;
-    unsigned char ra :1;
+    u_int16_t qr :1;
+    u_int16_t opcode :4;
+    u_int16_t aa :1;
+    u_int16_t tc :1;
+    u_int16_t rd :1;
+    u_int16_t ra :1;
+    u_int16_t z :3;
+    u_int16_t rcode :4;
 
-    unsigned short q_count;
-    unsigned short ans_count;
-    unsigned short auth_count;
-    unsigned short add_count;
+    u_int16_t qdcount;
+    u_int16_t ancount;
+    u_int16_t nscount;
+    u_int16_t arcount;
 } DNS_HEADER;
 
 typedef struct {
+    char* qname;
     unsigned short qtype;
     unsigned short qclass;
 } QUESTION;
+
+typedef struct {
+    char* name;
+    u_int16_t type;
+    u_int16_t class;
+    u_int32_t ttl;
+    u_int16_t rdlength;
+    char* rdata;
+} RESPONSE;
+
+typedef struct{
+    DNS_HEADER header;
+    QUESTION question;
+    char* data;
+    u_int16_t data_size;
+} DNS_PACKET;
 
 
 int main(void){
@@ -52,47 +81,46 @@ int main(void){
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in addr;
-    char sendBuff[1025];
+    struct sockaddr_in serv_addr, client_addr;
+    char buf[DNS_PACKET_SIZE+4];
+    socklen_t client_len;
     int listenfd = 0, conn = 0;
+
+    int data_size;
 
     if((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
         perror("Error with server socket creation");
         exit(EXIT_FAILURE);
     }
-    memset(&addr, '0', sizeof(addr));
-    memset(sendBuff, '0', sizeof(sendBuff));
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(buf, '0', sizeof(buf));
 
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(53);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(PORT);
 
-    if(bind(listenfd, (struct sockaddr*)&addr, sizeof(serv)) < 0){
+    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
         perror("Error with server socket binding");
         exit(EXIT_FAILURE);
     }
 
-    if(listen(listenfd, 10) < 0){
-        perror("Error with listening");
-        exit(EXIT_FAILURE);
-    }
-
     while(1){
-        if((conn = accept(listenfd, (struct sockaddr*)&addr, sizeof(addr))) < 0){
-            perror("Error with client connection");
+        if((data_size = recvfrom(listenfd, buf, DNS_PACKET_SIZE + 4, 0, (struct sockaddr*)&client_addr, &client_len))< 0){
+            perror("Error with data");
             exit(EXIT_FAILURE);
         }
 
+        write(1, buf, DNS_PACKET_SIZE+4);
 
     }
 
     printf("%s%s%s", upstreamServer, blackList, code);
+    close(listenfd);
 }
 
 
 /*
     TODO: 
-    Реализовать структуры заголовков DNS пакета
     Реализовать извлечение информации из получаемых сервером транзитных пакетов
     Реализовать фильтрацию по черному списку и возврат кода
 */
